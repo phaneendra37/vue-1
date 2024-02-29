35dca5835c4546b2c28c55acc5473e345cc8248f
phaneendra37 <kphaneendra337@gmail.com>
2024-03-01 02:25:21 +0530

    added the sidebar and modal component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <nav class="bg-green-500">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <button class="btn-primary" @click="sidebar = true">Open Sidebar</button>
+    <Canvas :isShow="sidebar" @close="toggleSidebar" @save="toggleSidebar" />
+  </div>
 </template>
+<script setup>
+import { ref } from "vue";
+import Canvas from "@/components/global/SlideOver.vue";
+const sidebar = ref(true);
+
+const toggleSidebar = (data) => {
+  console.log("okay");
+  sidebar.value = data;
+};
+</script>
