14f4515addbc24fe462aa313a2d2c5f73029585b
phaneendra37 <kphaneendra337@gmail.com>
2024-03-01 02:25:47 +0530

    added app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <button class="btn-primary" @click="sidebar = true">Open Sidebar</button>
-    <Canvas :isShow="sidebar" @close="toggleSidebar" @save="toggleSidebar" />
+  <div class="mt-4 flex items-center gap-3 px-6">
+    <button class="btn-primary" @click="(sidebar = true), (isSidebar = true)">
+      Open Sidebar
+    </button>
+    <button class="btn-primary" @click="(sidebar = true), (isSidebar = false)">
+      Open modal
+    </button>
+    <Canvas
+      :isShow="sidebar"
+      @close="toggleSidebar"
+      :isSidebar="isSidebar"
+      @save="toggleSidebar"
+    />
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
 import Canvas from "@/components/global/SlideOver.vue";
 const sidebar = ref(true);
-
+const isSidebar = ref(true);
 const toggleSidebar = (data) => {
-  console.log("okay");
   sidebar.value = data;
 };
 </script>
