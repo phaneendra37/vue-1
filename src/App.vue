05c85766183d7431df22ef93288f5b95685fcfd5
phani <64074232+phaneendra37@users.noreply.github.com>
2024-03-05 00:59:04 +0530

    Updated bugs (#24)
    * bug fixes
    
    * Added confetti

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="max-w-7xl mx-auto py-4 flex gap-x-6 items-center">
-    <infinityScroll v-bind="jsonData" />
+    <infinityScroll v-bind="jsonData" :field-json="FormFields" />
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { prideConfetti } from "@/js/Confetti.js";
+import FormFields from "@/json/sidebar.json";
 import infinityScroll from "./components/global/infinityScroll.vue";
 import infinityJson from "@/json/infinity.json";
 const jsonData = ref(infinityJson);
+onMounted(prideConfetti);
 </script>
