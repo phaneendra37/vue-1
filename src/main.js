31584b39c6f84aca50b927cbe1b4885f46260ba0
phaneendra37 <kphaneendra337@gmail.com>
2024-03-16 23:24:13 +0000

    New build was released

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import '../public/css/main.css'
-import 'vue3-toastify/dist/index.css';
-
-createApp(App).use(store).use(router).mount('#app')
